c1bf0198427e9db0b10bc3378c5b9d94c406b543
Clashsoft <clashsoft@hotmail.com>
2015-03-07 00:24:07 +0100

    Implementation

---
+++ src/com/clashsoft/loc/LoCThread.java
@@ -0,0 +1,97 @@
+package com.clashsoft.loc;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoCThread extends Thread
+{
+	protected static List<File>	files	= new ArrayList();
+	protected static int		dirCount;
+	protected static long		loc;
+	protected static long		sloc;
+	protected static long		cloc;
+	
+	public static void startThread(ActionEvent e)
+	{
+		new LoCThread().start();
+	}
+	
+	@Override
+	public void run()
+	{
+		files.clear();
+		dirCount = 0;
+		loc = sloc = cloc = 0L;
+		LoCTool.instance.progressBar.setString("Scanning for Files...");
+		LoCTool.instance.progressBar.setStringPainted(true);
+		walkFileTree(new File(LoCTool.instance.textFieldProjectRoot.getText()));
+		processFiles();
+	}
+	
+	public static void walkFileTree(File file)
+	{
+		if (file.isDirectory())
+		{
+			String name = file.getName();
+			if (name.startsWith("."))
+			{
+				return;
+			}
+			
+			dirCount++;
+			for (String s : file.list())
+			{
+				walkFileTree(new File(file, s));
+			}
+			return;
+		}
+		
+		files.add(file);
+	}
+	
+	public static void processFiles()
+	{
+		int count = files.size();
+		LoCTool.instance.progressBar.setString("0 / " + count);
+		LoCTool.instance.progressBar.setMinimum(0);
+		LoCTool.instance.progressBar.setMaximum(count);
+		LoCTool.instance.labelFileCount.setText("Files: " + count);
+		LoCTool.instance.labelDirCount.setText("Directories: " + dirCount);
+		
+		for (int i = 0; i < count; )
+		{
+			processFile(files.get(i));
+			LoCTool.instance.progressBar.setValue(count);
+			LoCTool.instance.progressBar.setString(++i + " / " + count);
+			LoCTool.instance.textFieldLOC.setText(Long.toString(loc));
+			LoCTool.instance.textFieldSLOC.setText(Long.toString(sloc));
+			LoCTool.instance.textFieldCLOC.setText(Long.toString(cloc));
+		}
+	}
+	
+	public static void processFile(File file)
+	{
+		try
+		{
+			for (String s : Files.readAllLines(file.toPath()))
+			{
+				loc++;
+				if (s.indexOf("//") == 0)
+				{
+					cloc++;
+				}
+				else
+				{
+					sloc++;
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+		}
+	}
+}
--- src/com/clashsoft/loc/LoCTool.java
+++ src/com/clashsoft/loc/LoCTool.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 public class LoCTool
 {
+	public static LoCTool		instance;
 	
 	private JFrame				frame;
 	public JButton				buttonSelectFile;
@@ -14,6 +15,7 @@ public class LoCTool
 	public JTextField			textFieldProjectRoot;
 	
 	private final JFileChooser	fileChooser	= new JFileChooser();
+	
 	public JButton				buttonScan;
 	public JLabel				labelLOC;
 	public JLabel				labelSLOC;
@@ -21,14 +23,17 @@ public class LoCTool
 	public JTextField			textFieldLOC;
 	public JTextField			textFieldSLOC;
 	public JTextField			textFieldCLOC;
+	public JProgressBar			progressBar;
+	public JLabel				labelFileCount;
+	public JLabel				labelDirCount;
 	
 	public static void main(String[] args)
 	{
 		EventQueue.invokeLater(() -> {
 			try
 			{
-				LoCTool window = new LoCTool();
-				window.frame.setVisible(true);
+				instance = new LoCTool();
+				instance.frame.setVisible(true);
 			}
 			catch (Exception e)
 			{
@@ -44,9 +49,11 @@ public class LoCTool
 	 */
 	public LoCTool()
 	{
+		this.fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		
 		this.frame = new JFrame();
 		this.frame.setTitle("LoC Tool");
-		this.frame.setBounds(100, 100, 450, 200);
+		this.frame.setBounds(100, 100, 450, 250);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.frame.getContentPane().setLayout(null);
 		
@@ -72,40 +79,50 @@ public class LoCTool
 		this.textFieldProjectRoot.setColumns(10);
 		
 		this.buttonScan = new JButton("Scan Lines of Code");
-		this.buttonScan.addActionListener(e -> {
-			
-		});
+		this.buttonScan.addActionListener(LoCThread::startThread);
 		this.buttonScan.setBounds(6, 39, 438, 29);
 		this.frame.getContentPane().add(this.buttonScan);
 		
 		this.labelLOC = new JLabel("Lines Of Code (LOC):");
-		this.labelLOC.setBounds(6, 80, 211, 16);
+		this.labelLOC.setBounds(6, 114, 211, 16);
 		this.frame.getContentPane().add(this.labelLOC);
 		
 		this.labelSLOC = new JLabel("Source Lines Of Code (SLOC):");
-		this.labelSLOC.setBounds(6, 108, 211, 16);
+		this.labelSLOC.setBounds(6, 142, 211, 16);
 		this.frame.getContentPane().add(this.labelSLOC);
 		
 		this.labelCLOC = new JLabel("Comment Lines Of Code (CLOC):");
-		this.labelCLOC.setBounds(6, 136, 211, 16);
+		this.labelCLOC.setBounds(6, 170, 211, 16);
 		this.frame.getContentPane().add(this.labelCLOC);
 		
 		this.textFieldLOC = new JTextField();
 		this.textFieldLOC.setEditable(false);
-		this.textFieldLOC.setBounds(229, 74, 215, 28);
+		this.textFieldLOC.setBounds(229, 108, 215, 28);
 		this.frame.getContentPane().add(this.textFieldLOC);
 		this.textFieldLOC.setColumns(10);
 		
 		this.textFieldSLOC = new JTextField();
 		this.textFieldSLOC.setEditable(false);
-		this.textFieldSLOC.setBounds(229, 102, 215, 28);
+		this.textFieldSLOC.setBounds(229, 136, 215, 28);
 		this.frame.getContentPane().add(this.textFieldSLOC);
 		this.textFieldSLOC.setColumns(10);
 		
 		this.textFieldCLOC = new JTextField();
 		this.textFieldCLOC.setEditable(false);
-		this.textFieldCLOC.setBounds(229, 130, 215, 28);
+		this.textFieldCLOC.setBounds(229, 164, 215, 28);
 		this.frame.getContentPane().add(this.textFieldCLOC);
 		this.textFieldCLOC.setColumns(10);
+		
+		this.progressBar = new JProgressBar();
+		this.progressBar.setBounds(6, 198, 438, 20);
+		this.frame.getContentPane().add(this.progressBar);
+		
+		this.labelFileCount = new JLabel("Files: -");
+		this.labelFileCount.setBounds(6, 80, 212, 16);
+		this.frame.getContentPane().add(this.labelFileCount);
+		
+		this.labelDirCount = new JLabel("Directory: -");
+		this.labelDirCount.setBounds(229, 80, 215, 16);
+		this.frame.getContentPane().add(this.labelDirCount);
 	}
 }
